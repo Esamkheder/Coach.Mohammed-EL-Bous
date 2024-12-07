dff84bea465ada32fb1b178a8fcd418940f347c7
Essam khedr <119887591+Esamkheder@users.noreply.github.com>
2024-12-07 17:40:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
+  //plans contents
+  function showContent(id) {
+    const contents = document.querySelectorAll('.tab-content');
+    contents.forEach(content => {
+        content.classList.remove('active');
+        content.style.opacity = '0';
+    });
+    const activeContent = document.getElementById(id);
+    activeContent.classList.add('active');
+    setTimeout(() => {
+        activeContent.style.opacity = '1';
+    }, 10);
+}
+
+
+//Alternatives Calculator contents 
+// وظيفة لعرض المحتوى بناءً على التاب المحدد
+function showContent(contentId) {
+
+  const allContents = document.querySelectorAll('.food_content');
+  allContents.forEach(content => content.classList.remove('active'));
+
+  const selectedContent = document.getElementById(contentId);
+  selectedContent.classList.add('active');
+
+  fillAlternativeFoodDropdown(contentId);
+}
+
+function fillAlternativeFoodDropdown(tabId) {
+  const dropdown = document.getElementById('select-alternative-food');
+  dropdown.innerHTML = '';
+
+  const selectedTabContent = document.getElementById(tabId);
+  const selectElement = selectedTabContent.querySelector('select');
+  
+  const options = selectElement.querySelectorAll('option');
+  
+
+  options.forEach(option => {
+      const optionElement = document.createElement('option');
+      optionElement.value = option.value;
+      optionElement.textContent = option.textContent;
+      dropdown.appendChild(optionElement);
+  });
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  showContent('content1'); 
+});
+
+
+
+//Certification
 new Swiper('.card-wrapper', {
     // Optional parameters
 
@@ -28,7 +82,8 @@ new Swiper('.card-wrapper', {
       } ,
     }
   
-  });
+});
+
   document.addEventListener("DOMContentLoaded", function () {
     const navLinks = document.querySelectorAll("nav ul li a");
     const checkbox = document.getElementById("check");
@@ -47,5 +102,3 @@ new Swiper('.card-wrapper', {
     loop:true
 
   });
-  
-  
